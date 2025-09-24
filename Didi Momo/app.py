9978357a1bf840b29592c314e70de0877dd9a092
Lucas Baruel <157977553+LucasBaruelCestaro@users.noreply.github.com>
2025-09-24 08:26:11 -0300

    Add files via upload

---
+++ Didi Momo/app.py
@@ -0,0 +1,3 @@
+from router.Router import Router
+
+Router.start(debug=True)
---
+++ Didi Momo/router/Router.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect
+
+class Router:
+    app = Flask(
+            import_name = __name__,
+            static_folder = '../static',
+            template_folder = '../templates'  
+        )
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+    
+    @staticmethod
+    def start(debug: bool = False):
+        return Router.app.run(debug=debug)
+
